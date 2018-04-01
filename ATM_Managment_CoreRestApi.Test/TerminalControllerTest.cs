a874509bedfc573bf039bbda51ad85e7028b2fbd
lezginaksoy <lezginaksoy@gmail.com>
2018-04-01 22:23:13 +0300

    unit test

---
+++ ATM_Managment_CoreRestApi.Test/TerminalControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using ATM_Management_CoreRestApi.Controllers;
+using ATM_Management_CoreRestApi.Data.Interface;
+using System;
+using System.Linq;
+
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ATM_Managment_CoreRestApi.Test
+{
+   public class TerminalControllerTest
+    {
+       // private readonly ITerminalRepository _TermRepo;
+
+        [Fact]
+        public void GetTerminalCountTest()
+        {
+            var Controller = new DefaultController();
+            var Result = Controller.Get();
+            Assert.Equal(3, Result.Count());           
+
+
+        }
+
+    }
+}
---
+++ ATM_Managment_CoreRestApi.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ATM_Managment_CoreRestApi.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
